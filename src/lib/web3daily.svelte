6e184d1a7edd56284bf6b522c49962d1cdee1249
Shooter <1235106+liushooter@users.noreply.github.com>
2022-07-30 18:37:04 +0000

    fetch

--- src/lib/web3daily.svelte
+++ src/lib/web3daily.svelte
@@ -1,21 +1,39 @@
+<script>
+  import { onMount } from "svelte";
+  // const endpoint = "https://jsonplaceholder.typicode.com/posts";
+    const url = "http://db.rebase.network/web3daily/public/posts?_order=-id,-time&_page_size=50&_page=1"
+
+    let posts = [];
+
+    const opts =  {
+      'Content-Type': 'application/json'
+    }
+
+    onMount(async function () {
+      const response = await fetch(url, {headers: opts});
+      console.log("response.status"  + response.status)
+      const data = await response.json();
+      console.log(data);
+      posts = data;
+    });
+
+</script>
+
+
 <div class="list-group list-group-checkable d-grid gap-2 border-0 w-auto">
-  <label class="list-group-item rounded-3 py-3" for="listGroupCheckableRadios1">
-    First radio
-    <span class="d-block small opacity-50">With support text underneath to add more detail</span>
+
+  {#each posts as post}
+  <div>
+    <p></p>
+  </div>
+  <label class="list-group-item rounded-3 py-3">
+    {post.title}
+    <span class="small opacity-50">{post.time}</span>
+    <span class="d-block small opacity-50">{post.url}</span>
+    <p>{post.introduce}</p>
   </label>
 
-  <label class="list-group-item rounded-3 py-3" for="listGroupCheckableRadios2">
-    Second radio
-    <span class="d-block small opacity-50">Some other text goes here</span>
-  </label>
+  {/each}
 
-  <label class="list-group-item rounded-3 py-3" for="listGroupCheckableRadios3">
-    Third radio
-    <span class="d-block small opacity-50">And we end with another snippet of text</span>
-  </label>
 
-  <label class="list-group-item rounded-3 py-3" for="listGroupCheckableRadios4">
-    Fourth disabled radio
-    <span class="d-block small opacity-50">This option is disabled</span>
-  </label>
 </div>
